8f50251d5c05dbf141c3653a36e56fda019abd80
Audre <audrewow@gmail.com>
2016-04-27 16:52:38 -0400

    Add search filter. Add comments. Add button confirmation on delete.

--- cis237Assignment6/Controllers/BeveragesController.cs
+++ cis237Assignment6/Controllers/BeveragesController.cs
@@ -10,6 +10,8 @@ using cis237Assignment6.Models;
 
 namespace cis237Assignment6.Controllers
 {
+    // Set as authorize so that all of this can't be accessed without being logged in
+    [Authorize]
     public class BeveragesController : Controller
     {
         private BeverageAStaffenEntities db = new BeverageAStaffenEntities();
@@ -17,7 +19,59 @@ namespace cis237Assignment6.Controllers
         // GET: Beverages
         public ActionResult Index()
         {
-            return View(db.Beverages.ToList());
+            // Variable to hold the beverage data
+            DbSet<Beverage> BeveragesToFilter = db.Beverages;
+
+            // Variables to hold the data from the session
+            string filterName = "";
+            string filterPack = "";
+            string filterMin = "";
+            string filterMax = "";
+
+            // Min and max for the prices
+            int min = 0;
+            int max = 99999;
+
+            // If there is something that is not just spaces in the session "name," assign to the corresponding variable
+            if (Session["name"] != null && !String.IsNullOrWhiteSpace((string)Session["name"]))
+            {
+                filterName = (string)Session["name"];
+            }
+            // If there is something that is not just spaces in the session "pack," assign to the corresponding variable
+            if (Session["pack"] != null && !String.IsNullOrWhiteSpace((string)Session["pack"]))
+            {
+                filterPack = (string)Session["pack"];
+            }
+            // If there is something that is not just spaces in the session "min," assign to the corresponding variable
+            if (Session["min"] != null && !String.IsNullOrWhiteSpace((string)Session["min"]))
+            {
+                filterMin = (string)Session["min"];
+                Int32.TryParse(filterMin, out min);
+            }
+            // If there is something that is not just spaces in the session "max," assign to the corresponding variable
+            if (Session["max"] != null && !String.IsNullOrWhiteSpace((string)Session["max"]))
+            {
+                filterMax = (string)Session["max"];
+                Int32.TryParse(filterMax, out max);
+            }
+
+            // Assign to filtered any beverages that are greater than or equal to the min, less than or equal to the max,
+            // contain the filterName, and contain the filterPack.
+            IEnumerable<Beverage> filtered = BeveragesToFilter.Where(beverage => beverage.price >= min &&
+                                                                                 beverage.price <= max &&
+                                                                                 beverage.name.Contains(filterName) &&
+                                                                                 beverage.pack.Contains(filterPack));
+            // Assign filtered to the list finalFiltered
+            IEnumerable<Beverage> finalFiltered = filtered.ToList();
+
+            // Assign the filter info to the viewbag so they can be used in the view to display the filter info in the text boxes
+            ViewBag.filterName = filterName;
+            ViewBag.filterPack = filterPack;
+            ViewBag.filterMin = filterMin;
+            ViewBag.filterMax = filterMax;
+
+            // Return the view of the filtered beverages
+            return View(finalFiltered);
         }
 
         // GET: Beverages/Details/5
@@ -122,6 +176,26 @@ namespace cis237Assignment6.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        // Filter method that can only redirect to the index from a form request. Sets up the info to be filtered.
+        [HttpPost]
+        public ActionResult Filter()
+        {
+            // Gets the data filter info from the form. 
+            string name = Request.Form.Get("name");
+            string pack = Request.Form.Get("pack");
+            string min = Request.Form.Get("min");
+            string max = Request.Form.Get("max");
+
+            // Store the form data in the session to be filtered later
+            Session["name"] = name;
+            Session["pack"] = pack;
+            Session["min"] = min;
+            Session["max"] = max;
+
+            // Redirect the user to the index page
+            return RedirectToAction("Index");
         }
     }
 }
